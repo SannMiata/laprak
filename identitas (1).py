e3815e918de8d3e584c2eff3604a51e3e2808f9e
SannMiata <coc12supercel@gmail.com>
2026-08-22 21:58:21 +0700

    Add files via upload

---
+++ identitas (1).py
@@ -0,0 +1,31 @@
+#Program PYTHON Identitas Diri
+nama = "Muhammad Ichsan" #Nama lengkap
+nim = "2605060027" #NIM
+ttl = "Magelang, 14 Juni 2007" #Tempat/Tanggal Lahir
+jk = "Laki-laki" #Jenis Kelamin
+hobi = "Olahraga, catur, dan mendengarkan musik" #Hobi
+
+print (f"\n\t\t\t\"Identitas Diri\"")
+print (f"Nama                    : {nama}")
+print (f"NIM                     : {nim}")
+print (f"Tempat/Tanggal Lahir    : {ttl}")
+print (f"Jenis Kelamin           : {jk}")
+print (f"Hobi                    : {hobi}")
+
+
+
+
+
+
+
+
+
+
+
+
+print ("\n\t\t\t\"Identitas Diri\"")
+print ("Nama                    : " + nama)
+print ("NIM                     : " + nim)
+print ("Tempat/Tanggal Lahir    : " + ttl)
+print ("Jenis Kelamin           : " + jk)
+print ("Hobi                    : " + hobi)
